5b9950dd365b8512095a320b0e02a9e2144e20fe
Minh Tai <dangminhtai15052002@gmail.com>
2025-06-05 11:44:40 +0700

    initial commit 3

---
+++ .eslintrc.prepublish.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['plugin:n8n-nodes-base/nodes'],
+  rules: {
+    'n8n-nodes-base/node-param-operation-option-action': 'off',
+    'n8n-nodes-base/node-param-operation-option-resource': 'off',
+  },
+}; 
